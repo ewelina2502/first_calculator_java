b1a699cea038c0598de56c78dad1cf72ef08bf4b
Ewelina <ewelina2502@gmail.com>
2021-01-21 10:27:05 +0100

    age checker

--- src/AgeChecker.java
+++ src/AgeChecker.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
 public class AgeChecker {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write your age:");
+        int age = scanner.nextInt();
+
+        if(age < 18 && age >= 0) {
+            System.out.println("You can't buy alkohol");
+        } else if (age < 0 ) {
+            System.out.println("Please write correct age");
+        } else
+            System.out.println("Thank you for shopping");
+    }
 }
