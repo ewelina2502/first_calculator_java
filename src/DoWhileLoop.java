2aa5413c472868f2ed374c7cc134ab21664b560f
Ewelina <ewelina2502@gmail.com>
2021-02-09 12:16:12 +0100

    do while loop

---
+++ src/DoWhileLoop.java
@@ -0,0 +1,11 @@
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+        int number = 21;
+        do {
+            System.out.println("My value is: " + number + " and I'm smaller than 20");
+            number++;
+        }
+        while (number <20);
+    }
+}
--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -1,7 +1,8 @@
 public class WhileLoop {
 
     public static void main(String[] args) {
-        int number=0;
+        int number= 0;
+
         while (number < 20) {
             System.out.println("My value: " + number + " and is smaller than 20");
             number++;
