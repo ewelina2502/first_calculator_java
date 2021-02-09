10afb4ed7e3238927de2856335316e85c52f595e
Ewelina <ewelina2502@gmail.com>
2021-02-09 12:07:45 +0100

    while loop

---
+++ src/WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        int number=0;
+        while (number < 20) {
+            System.out.println("My value: " + number + " and is smaller than 20");
+            number++;
+        }
+    }
+}
