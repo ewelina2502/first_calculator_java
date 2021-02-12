df1fd0ce3244042d0a7d47bdf4b5745a2cd11513
Ewelina <ewelina2502@gmail.com>
2021-02-12 14:13:53 +0100

    methods and appeal ver 2.0

--- src/Metody.java
+++ src/Metody.java
@@ -2,14 +2,14 @@ public class Metody {
 
     //void nie zwraca wartości
 
-    public void policzWynik() {
-        System.out.println("Zaraz policzę wynik");
-        int result = 0;
-        for (int i = 0; i < 100; i++) {
-            result = result + i;
-        }
-        System.out.println("Rezultat to: " + result);
-    }
+//    public void policzWynik() {
+//        System.out.println("Zaraz policzę wynik");
+//        int result = 0;
+//        for (int i = 0; i < 100; i++) {
+//            result = result + i;
+//        }
+//        System.out.println("Rezultat to: " + result);
+//    }
 
     public int pobierzWynik() {
         System.out.println("Zaraz policzę wynik");
@@ -20,4 +20,21 @@ public class Metody {
         System.out.println("Rezultat to: " + result);
         return result;
     }
+
+    public void policzWynikParam(int number) {
+        System.out.println("Number ma wartosc: " + number);
+        System.out.println("Zaraz policzę wynik");
+        int result = 0;
+        for (int i = 0; i < number; i++) {
+            result = result + i;
+        }
+            System.out.println("Rezultat to: " + result);
+        }
+
+    public int add(int firstNumber, int secondNumber, String word) {
+//        System.out.println("Suma to: " + (firstNumber + secondNumber));
+        System.out.println(word);
+        return firstNumber + secondNumber;
+    }
 }
+
--- src/MetodyTest.java
+++ src/MetodyTest.java
@@ -2,11 +2,22 @@ public class MetodyTest {
 
     public static void main(String[] args) {
         Metody metody = new Metody();
+
 //        metody.policzWynik();
-        int result = metody.pobierzWynik();
-        int result2 = result *2;
-        System.out.println("Resultat przed mnożeniem to: " + result);
-        System.out.println("Resultat po mnożeniu to: " + result2);
+//        int result = metody.pobierzWynik();
+//        int result2 = result *2;
+//        System.out.println("Resultat przed mnożeniem to: " + result);
+//        System.out.println("Resultat po mnożeniu to: " + result2);
+
+
+//        metody.policzWynikParam( 2);
+//        metody.policzWynikParam( 100);
+//        metody.policzWynikParam( 23);
+
+        int result = metody.add(2,3, "Hello");
+        System.out.println(metody.add(2,6, "Hello2"));
+        System.out.println(result);
+
 
     }
 }
