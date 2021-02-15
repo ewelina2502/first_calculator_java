cbe1f2564a6a3a4f87beab01ee9a0fb0f1aca69b
Ewelina <ewelina2502@gmail.com>
2021-02-15 10:12:41 +0100

    constructors

---
+++ src/User.java
@@ -0,0 +1,17 @@
+public class User {
+
+    public String usename;
+    public String password;
+
+//    public User() {
+//
+//    }
+//
+//    public User(String user, String pass) {
+//        System.out.println("Hello z konstruktora");
+//    }
+
+    public void sayHello() {
+        System.out.println("Hello my name is " + usename);
+    }
+}
---
+++ src/UserTest.java
@@ -0,0 +1,12 @@
+public class UserTest {
+
+    public static void main(String[] args) {
+        User user = new User();
+//        User user2 = new User( "hello", "Bartek");
+
+        user.usename = "Bartek";
+        user.password = "QwWERY";
+        user.sayHello();
+
+    }
+}
