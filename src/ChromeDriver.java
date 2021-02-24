cd4ce0c153cf428296d108c561583eeaba112fe2
Ewelina <ewelina2502@gmail.com>
2021-02-24 10:28:54 +0100

    Homework @override WebDrivers

---
+++ src/ChromeDriver.java
@@ -0,0 +1,12 @@
+public class ChromeDriver implements WebDriver {
+
+    @Override
+    public void get() {
+        System.out.println("Open Chrome Driver");
+    }
+
+    @Override
+    public void findElementBy() {
+        System.out.println("Find element by Chrome Driver");
+    }
+}
---
+++ src/FirefoxDriver.java
@@ -0,0 +1,13 @@
+public class FirefoxDriver implements WebDriver {
+
+    @Override
+    public void get() {
+        System.out.println("Open FF Driver");
+    }
+
+    @Override
+    public void findElementBy() {
+        System.out.println("Find element by FF Driver");
+
+    }
+}
