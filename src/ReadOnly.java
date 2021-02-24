442fb093d9c902f97bbb88dd5c68dc08aa04ffe6
Ewelina <ewelina2502@gmail.com>
2021-02-24 10:04:05 +0100

    getter + setter: getter umożliwia odczyt wartość z pola prywatnego, setter umożliwia konfigurację wartości na polu prywatnym, ale nie można wykonać żadnego działania

---
+++ src/ReadOnly.java
@@ -0,0 +1,24 @@
+public class ReadOnly {
+
+    private String name = "Bartek";
+
+//    public String getName() {
+//        return  name + "jest ok";
+//    }
+
+//    public void setName(String name) {
+//        this.name = name;
+//
+//
+//    }
+//              skrót klawiszowy: alt + insert
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
