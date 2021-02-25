85fbc6fa1a454439d096e6c9a573742e160dbeb9
Ewelina <ewelina2502@gmail.com>
2021-02-25 11:55:43 +0100

    polimorfizm  - modyfikacja kodu

--- src/drivers/WebDriverTest.java
+++ src/drivers/WebDriverTest.java
@@ -3,13 +3,29 @@ package drivers;
 public class WebDriverTest {
 
     public static void main(String[] args) {
-        ChromeDriver chrome = new ChromeDriver();
-        chrome.get();
-        chrome.findElementBy();
+        WebDriver driver = getDriver("firefox");
+        driver.get();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
+        driver.findElementBy();
 
-        FirefoxDriver firefox = new FirefoxDriver();
-        firefox.get();
-        firefox.findElementBy();
+//        FirefoxDriver firefox = new FirefoxDriver();
+//        firefox.get();
+//        firefox.findElementBy();
+//
+//    }
 
+    }
+
+    private static WebDriver getDriver(String name) {
+        if(name.equals("chrome")) {
+            return new ChromeDriver();
+        }
+        else if (name.equals("firefox")) {
+            return new FirefoxDriver();
+        }
+        return null;
     }
 }
