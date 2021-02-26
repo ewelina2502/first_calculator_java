e0b8bf8ee819920757dc700938a6161900b5b451
Ewelina <ewelina2502@gmail.com>
2021-02-26 10:49:52 +0100

    exception handling ver 2.0

--- src/CheckedExceptions.java
+++ src/CheckedExceptions.java
@@ -3,8 +3,18 @@ import java.io.FileNotFoundException;
 
 public class CheckedExceptions {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        readFile("/src/ewe.txt");
+    public static void main(String[] args)  {
+        try {
+            System.out.println("Before reading file");
+            readFile("C:\\Users\\Ewelina\\first_project_java\\src\\tests.txt");
+            System.out.println("Close file");
+        } catch (FileNotFoundException e) {
+            System.out.println("Wyjątek został wyrzucony");
+            System.out.println(e.getMessage());
+        }
+        finally { //wykona się zawsze
+            System.out.println("Closing file");
+        }
     }
 
     public static void readFile(String path) throws FileNotFoundException {
