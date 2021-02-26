f0b8ce2360830018769d24bc14115a06494b9cf4
Ewelina <ewelina2502@gmail.com>
2021-02-26 10:35:46 +0100

    exception handling

---
+++ src/CheckedExceptions.java
@@ -0,0 +1,16 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class CheckedExceptions {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        readFile("/src/ewe.txt");
+    }
+
+    public static void readFile(String path) throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream(path);
+
+
+
+    }
+}
